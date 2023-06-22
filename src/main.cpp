6b46797ed0b45155b36ae8ae4a63e2ed923943f6
zdrvzbl <sanya2003ogorodov@gmail.com>
2023-06-23 00:03:03 +0700

    add stbi headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers" 
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_ONLY_JPEG
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+#pragma GCC diagnostic pop
+
 #include <iostream>
 
 int main()
